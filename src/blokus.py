92023a826406f8a16fff03e97a1781cbcdea1d59
Logan Rouleau <loganrouleau@users.noreply.github.com>
2020-01-05 19:19:02 -0800

    Separated picker grids by player

--- src/blokus.py
+++ src/blokus.py
@@ -9,12 +9,13 @@ BOARD_SIZE_PX = BOARD_SIZE_CELLS * CELL_SIZE_PX
 DIVIDER_WIDTH_PX = 36
 PICKER_HEIGHT_OFFSET_PX = 2 * CELL_SIZE_PX
 PICKER_WIDTH_OFFSET_PX = BOARD_SIZE_PX + DIVIDER_WIDTH_PX
+PICKER_CELL_SIZE_PX = 18
 PICKER_ROWS = 12
 PICKER_COLS = 23
-PICKER_HEIGHT_PX = PICKER_ROWS * CELL_SIZE_PX
-PICKER_WIDTH_PX = PICKER_COLS * CELL_SIZE_PX
-CANVAS_WIDTH_PX = (BOARD_SIZE_CELLS + PICKER_COLS) * \
-    CELL_SIZE_PX + DIVIDER_WIDTH_PX
+PICKER_HEIGHT_PX = PICKER_ROWS * PICKER_CELL_SIZE_PX
+PICKER_WIDTH_PX = PICKER_COLS * PICKER_CELL_SIZE_PX
+CANVAS_WIDTH_PX = (BOARD_SIZE_CELLS * CELL_SIZE_PX) + \
+    (PICKER_COLS * PICKER_CELL_SIZE_PX) + DIVIDER_WIDTH_PX
 CANVAS_HEIGHT_PX = BOARD_SIZE_CELLS * CELL_SIZE_PX
 PLAYERS = {0: "red", 1: "green"}
 
@@ -24,8 +25,8 @@ tiles = [[None for _ in range(BOARD_SIZE_CELLS)]
          for _ in range(BOARD_SIZE_CELLS)]
 picker_tiles = [[None for _ in range(PICKER_COLS)]
                 for _ in range(PICKER_ROWS)]
-picker_blocks = {"red": block_generator.generate_blocks(
-), "green": block_generator.generate_blocks()}
+picker_blocks = {"red": block_generator.generate_blocks(),
+                 "green": block_generator.generate_blocks()}
 
 move_flag = False
 mouse_xpos = -1
@@ -34,14 +35,9 @@ mouse_ypos = -1
 
 def switch_player():
     global current_player
-    if current_player == 0:
-        current_player = 1
-    else:
-        current_player = 0
+    current_player = 1 if current_player == 0 else 0
     global selected_block
     selected_block = "-1"
-    for item in canvas.find_withtag("picker"):
-        canvas.itemconfig(item, fill=PLAYERS[current_player])
 
 
 def paint_board():
@@ -58,7 +54,7 @@ def paint_board():
 
 def configure_canvas(event=None):
     draw_board_grid()
-    draw_picker_blocks(current_player)
+    draw_picker_blocks()
 
 
 def draw_board_grid():
@@ -70,34 +66,36 @@ def draw_board_grid():
             [(0, i), (BOARD_SIZE_PX, i)])
 
 
-def draw_picker_blocks(player):
-    for current_block in picker_blocks[PLAYERS[player]]:
-        for coord in current_block.coordinates:
-            canvas.create_rectangle(
-                PICKER_WIDTH_OFFSET_PX + (
-                    current_block.position[1] + coord[1])*CELL_SIZE_PX,
-                PICKER_HEIGHT_OFFSET_PX + (
-                    current_block.position[0] + coord[0])*CELL_SIZE_PX,
-                PICKER_WIDTH_OFFSET_PX + (
-                    current_block.position[1] + coord[1] + 1)*CELL_SIZE_PX,
-                PICKER_HEIGHT_OFFSET_PX + (
-                    current_block.position[0] + coord[0] + 1)*CELL_SIZE_PX,
-                fill=PLAYERS[current_player], tags=(PLAYERS[player] + "_block_" + str(current_block.index), "picker"))
+def draw_picker_blocks():
+    height_offset = PICKER_HEIGHT_OFFSET_PX
+    for player in PLAYERS:
+        for current_block in picker_blocks[PLAYERS[player]]:
+            for coord in current_block.coordinates:
+                canvas.create_rectangle(
+                    PICKER_WIDTH_OFFSET_PX + (
+                        current_block.position[1] + coord[1])*PICKER_CELL_SIZE_PX,
+                    height_offset + (
+                        current_block.position[0] + coord[0])*PICKER_CELL_SIZE_PX,
+                    PICKER_WIDTH_OFFSET_PX + (
+                        current_block.position[1] + coord[1] + 1)*PICKER_CELL_SIZE_PX,
+                    height_offset + (
+                        current_block.position[0] + coord[0] + 1)*PICKER_CELL_SIZE_PX,
+                    fill=PLAYERS[player], tags=(PLAYERS[player] + "_block_" + str(current_block.index), "picker"))
+        height_offset += PICKER_HEIGHT_PX
 
 
-def on_block_selection(event=None):
+def on_block_selection(event):
     tags = canvas.gettags("current")
-    if not tags:
+    if not tags or not tags[0].split("_")[0] == PLAYERS[current_player]:
         return
     global selected_block
     selected_block = tags[0]
+    for item in canvas.find_withtag(selected_block):
+        canvas.scale(item, event.x, event.y, 2, 2)
 
 
 def on_move(event):
-    global move_flag
-    global mouse_xpos
-    global mouse_ypos
-    global selected_block
+    global move_flag, mouse_xpos, mouse_ypos, selected_block
     if move_flag:
         new_xpos, new_ypos = event.x, event.y
         for item in canvas.find_withtag(selected_block):
@@ -113,6 +111,12 @@ def on_move(event):
 
 
 def on_release(event):
+    if selected_block == "-1":
+        return
+    if event.x > BOARD_SIZE_PX + DIVIDER_WIDTH_PX:
+        for item in canvas.find_withtag(selected_block):
+            canvas.scale(item, event.x, event.y, 0.5, 0.5)
+
     global move_flag
     move_flag = False
     col = int(event.x/CELL_SIZE_PX)
@@ -122,13 +126,13 @@ def on_release(event):
     for coord in picker_blocks[PLAYERS[current_player]][int(selected_block.split("_")[2])].coordinates:
         proposed_coordinates.append([row + coord[0], col + coord[1]])
     global tiles
-    # if validator.placement_is_valid(tiles, proposed_coordinates, current_player):
-    for coord in proposed_coordinates:
-        tiles[coord[0]][coord[1]] = current_player
-    for item in canvas.find_withtag(selected_block):
-        canvas.delete(item)
-    paint_board()
-    switch_player()
+    if validator.placement_is_valid(tiles, proposed_coordinates, current_player):
+        for coord in proposed_coordinates:
+            tiles[coord[0]][coord[1]] = current_player
+        for item in canvas.find_withtag(selected_block):
+            canvas.delete(item)
+        paint_board()
+        switch_player()
 
 
 root = tk.Tk()
