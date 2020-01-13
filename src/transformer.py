a17b4b88a7b675e05df2ed2c4437addc7decc8af
Logan Rouleau <loganrouleau@users.noreply.github.com>
2020-01-12 20:25:29 -0800

    Prevent selected segment from moving on block rotation

--- src/transformer.py
+++ src/transformer.py
@@ -5,5 +5,5 @@ def rotate_90(selected_block, selected_segment):
     offset = selected_block.coordinates[int(selected_segment.split("_")[2])]
     new_coords = []
     for coord in selected_block.coordinates:
-        new_coords.append([-(coord[1] - offset[0]), coord[0] - offset[1]])
+        new_coords.append([-(coord[1] - offset[1]), coord[0] - offset[0]])
     return block.Block(selected_block.index, selected_block.position, new_coords)
--- test/test_transformer.py
+++ test/test_transformer.py
@@ -5,6 +5,11 @@ from src import block
 
 class TestTransformer(unittest.TestCase):
 
+    def test_rotated_block_has_origin_at_selected_segment(self):
+        input_block = block.Block(0, [0, 3], [[0, 0], [0, 1]])
+        self.assertEqual(transformer.rotate_90(
+            input_block, "block_segment_1").coordinates[1], [0, 0])
+
     def test_rotate_single_coord_block(self):
         input_block = block.Block(0, [0, 3], [[0, 0]])
         expected_coords = [[0, 0]]
@@ -23,9 +28,19 @@ class TestTransformer(unittest.TestCase):
     def test_rotate_long_straight_block_from_middle(self):
         input_block = block.Block(
             14, [5, 22], [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]])
-        expected_coords = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]]
+        expected_coords = [[0, -1], [0, 0], [0, 1], [0, 2], [0, 3]]
         actual_coords = transformer.rotate_90(
             input_block, "block_segment_1").coordinates
+        self.assertEqual(actual_coords, expected_coords)
+
+    def test_rotating_4_times_should_result_in_same_shape(self):
+        input_block = block.Block(5, [2, 4], [[0, 1], [1, 0], [1, 1], [1, 2]])
+        expected_coords = [[-1, 0], [0, -1], [0, 0], [0, 1]]
+        actual_block = transformer.rotate_90(input_block, "block_segment_2")
+        actual_block = transformer.rotate_90(actual_block, "block_segment_2")
+        actual_block = transformer.rotate_90(actual_block, "block_segment_2")
+        actual_block = transformer.rotate_90(actual_block, "block_segment_2")
+        actual_coords = actual_block.coordinates
         self.assertEqual(actual_coords, expected_coords)
 
 
