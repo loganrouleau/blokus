c7d479b059c2a374863267195f08b403e7521f2a
Logan Rouleau <loganrouleau@users.noreply.github.com>
2020-01-25 18:31:41 -0800

    Fix transformer unit tests

--- test/test_transformer.py
+++ test/test_transformer.py
@@ -9,18 +9,18 @@ class TestTransformer(unittest.TestCase):
     def test_rotated_block_has_origin_at_selected_segment(self):
         input_block = Block(0, [0, 3], [[0, 0], [0, 1]])
         self.assertEqual(transformer.rotate_90(
-            input_block, "block_segment_1").coordinates[1], [0, 0])
+            input_block, 1).coordinates[1], [0, 0])
 
     def test_flipped_block_has_origin_at_selected_segment(self):
         input_block = Block(0, [0, 3], [[0, 0], [0, 1]])
         self.assertEqual(transformer.flip(
-            input_block, "block_segment_1").coordinates[1], [0, 0])
+            input_block, 1).coordinates[1], [0, 0])
 
     def test_rotate_single_coord_block(self):
         input_block = Block(0, [0, 3], [[0, 0]])
         expected_coords = [[0, 0]]
         actual_coords = transformer.rotate_90(
-            input_block, "block_segment_0").coordinates
+            input_block, 0).coordinates
         self.assertEqual(actual_coords, expected_coords)
 
     def test_rotate_long_straight_block_from_end(self):
@@ -28,7 +28,7 @@ class TestTransformer(unittest.TestCase):
             14, [5, 22], [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]])
         expected_coords = [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]]
         actual_coords = transformer.rotate_90(
-            input_block, "block_segment_0").coordinates
+            input_block, 0).coordinates
         self.assertEqual(actual_coords, expected_coords)
 
     def test_rotate_long_straight_block_from_middle(self):
@@ -36,16 +36,16 @@ class TestTransformer(unittest.TestCase):
             14, [5, 22], [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]])
         expected_coords = [[0, -1], [0, 0], [0, 1], [0, 2], [0, 3]]
         actual_coords = transformer.rotate_90(
-            input_block, "block_segment_1").coordinates
+            input_block, 1).coordinates
         self.assertEqual(actual_coords, expected_coords)
 
     def test_rotating_4_times_should_result_in_same_shape(self):
         input_block = Block(5, [2, 4], [[0, 1], [1, 0], [1, 1], [1, 2]])
         expected_coords = [[-1, 0], [0, -1], [0, 0], [0, 1]]
-        actual_block = transformer.rotate_90(input_block, "block_segment_2")
-        actual_block = transformer.rotate_90(actual_block, "block_segment_2")
-        actual_block = transformer.rotate_90(actual_block, "block_segment_2")
-        actual_block = transformer.rotate_90(actual_block, "block_segment_2")
+        actual_block = transformer.rotate_90(input_block, 2)
+        actual_block = transformer.rotate_90(actual_block, 2)
+        actual_block = transformer.rotate_90(actual_block, 2)
+        actual_block = transformer.rotate_90(actual_block, 2)
         actual_coords = actual_block.coordinates
         self.assertEqual(actual_coords, expected_coords)
 
@@ -54,7 +54,7 @@ class TestTransformer(unittest.TestCase):
             18, [9, 11], [[1, 0], [0, 1], [1, 1], [2, 1], [0, 2]])
         expected_coords = [[1, 1], [0, 0], [1, 0], [2, 0], [0, -1]]
         actual_coords = transformer.flip(
-            input_block, "block_segment_1").coordinates
+            input_block, 1).coordinates
         self.assertEqual(actual_coords, expected_coords)
 
 
